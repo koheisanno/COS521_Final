5f66af7017343eb21e3428ba8118ebf898899b1e
koheisanno <kohei@gmail.com>
2023-12-08 16:53:44 -0500

    initial commit

--- lsh.py
+++ lsh.py
@@ -1,40 +1,39 @@
 import numpy as np
+from collections import defaultdict
 
 class LSH:
     '''
     random projection based locality sensitive hashing.
 
     Attributes:
-    - hash_size: the length of the hash output
     - input_dim: the dimension of input data
     - seed: random seed (used to replicate results)
-    - projection_plane: random projection plane; hash_size x input_dim numpy array
+    - projection_line: random projection line; input_dim x 1 numpy array
     - rng: a random number generator
     '''
-    def __init__(self, hash_size, input_dim, bucket_size, seed=None):
-        self.hash_size = hash_size
+    def __init__(self, input_dim, bucket_size, seed=None):
         self.input_dim = input_dim
         self.seed = seed
         self.bucket_size = bucket_size
-        self.hash_table = {}
+        self.hash_table = defaultdict(list)
 
         if self.seed is not None:
             self.rng = np.random.Generator(seed=self.seed)
         else:
             self.rng = np.random.default_rng()
 
-        self.projection_plane = self._generate_plane()
+        self.projection_line = self._generate_plane()
 
     def _generate_plane(self):
         '''
         Generate a random plane for projection.
         '''
-        return self.rng.standard_normal(size=(self.hash_size, self.input_dim))
+        return self.rng.standard_normal(self.input_dim)
     
     def _hash(self, point):
-        projection = np.dot(self.projection_plane, point)
+        projection = np.dot(self.projection_line, point)
 
-        return projection // self.bucket_size
+        return projection
     
     def index(self, point):
         '''
@@ -42,6 +41,6 @@ class LSH:
         '''
         hash_index = self._hash(point)
 
-        self.hash_table[hash_index] = tuple(point)
+        self.hash_table[hash_index].append(tuple(point))
 
         return hash_index
